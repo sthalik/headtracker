b7d085a3a6abd404d22d2adb9788f63052e6bf6f
Stanisław Halik <sthalik@misaki.pl>
2013-02-01 02:24:16 +0100

    Use compat definitions for SHM on non-win32, enabling FTNOIR

--- ftnoir/main.cpp
+++ ftnoir/main.cpp
@@ -1,45 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <windows.h>
-#include <tchar.h>
 #include <ht-api.h>
 #include "headtracker-ftnoir.h"
-
-TCHAR shmName[] = TEXT(HT_SHM_NAME);
-TCHAR mutexName[] = TEXT(HT_MUTEX_NAME);
+#include "compat.h"
 
 int main(void)
 {
-    HANDLE hMapFile = OpenFileMapping(FILE_MAP_ALL_ACCESS, false, shmName);
-
-    if (!hMapFile)
-    {
-        printf("Can't create file mapping\n");
-        return 1;
-    }
-
-    ht_shm_t* shm = (ht_shm_t*) MapViewOfFile(hMapFile,
-                                              FILE_MAP_READ | FILE_MAP_WRITE,
-                                              0,
-                                              0,
-                                              sizeof(ht_shm_t));
-
-    if (!shm)
-    {
-        (void) CloseHandle(hMapFile);
-        printf("Can't map view of file\n");
-        return 1;
-    }
-
-    HANDLE hMutex = OpenMutex(SYNCHRONIZE, false, mutexName);
-
-    if (!hMutex)
-    {
-        UnmapViewOfFile(shm);
-        CloseHandle(hMapFile);
-        printf("Can't open mutex\n");
-        return 1;
-    }
+	ht_shm_t* shm;
+	PortableLockedShm lck_shm(HT_SHM_NAME, HT_MUTEX_NAME, sizeof(ht_shm_t));
+	
+	if ((shm = (ht_shm_t*) lck_shm.mem) == NULL)
+	{
+		fprintf(stderr, "Oh, bother\n");
+		return 1;
+	}
 
     shm->running = true;
 
@@ -56,30 +30,24 @@ int main(void)
         }
         if (!ht_cycle(ctx, &result))
             break;
-        if (WaitForSingleObject(hMutex, INFINITE) == WAIT_OBJECT_0)
-        {
-            shm->result = result;
-            ht_get_bgr_frame(ctx, &frame);
-            if (frame.cols <= HT_MAX_VIDEO_WIDTH && frame.rows <= HT_MAX_VIDEO_HEIGHT && frame.channels <= HT_MAX_VIDEO_CHANNELS)
-            {
-                memcpy(shm->frame.frame, frame.data, frame.cols * frame.rows * frame.channels);
-                shm->frame.channels = frame.channels;
-                shm->frame.width = frame.cols;
-                shm->frame.height = frame.rows;
-            }
-            ReleaseMutex(hMutex);
-            if (frame.data)
-                delete[] frame.data;
-        }
+        lck_shm.lock();
+		shm->result = result;
+		ht_get_bgr_frame(ctx, &frame);
+		if (frame.cols <= HT_MAX_VIDEO_WIDTH && frame.rows <= HT_MAX_VIDEO_HEIGHT && frame.channels <= HT_MAX_VIDEO_CHANNELS)
+		{
+			memcpy(shm->frame.frame, frame.data, frame.cols * frame.rows * frame.channels);
+			shm->frame.channels = frame.channels;
+			shm->frame.width = frame.cols;
+			shm->frame.height = frame.rows;
+		}
+		lck_shm.unlock();
+		if (frame.data)
+			delete[] frame.data;
     }
 
     shm->running = false;
 
     ht_free_context(ctx);
-
-    UnmapViewOfFile(shm);
-    CloseHandle(hMapFile);
-    CloseHandle(hMutex);
 
     return 0;
 }
