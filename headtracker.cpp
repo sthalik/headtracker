928f33074ce5330ca4e5453529ba2a315f05e130
Stanisław Halik <sthalik@misaki.pl>
2013-02-06 03:52:39 +0100

    Compute focal length for vertical properly

--- headtracker.cpp
+++ headtracker.cpp
@@ -110,7 +110,9 @@ HT_API(bool) ht_cycle(headtracker_t* ctx, ht_result_t* euler) {
         if (!(ctx->focal_length_w > 0)) {
             ctx->focal_length_w = ctx->grayscale.cols / tan(0.5 * ctx->config.field_of_view * HT_PI / 180);
             //ctx->focal_length_h = ctx->focal_length_w;
-            ctx->focal_length_h = ctx->grayscale.rows / tan(0.5 * ctx->config.field_of_view * (ctx->grayscale.rows / (float) ctx->grayscale.cols) * HT_PI / 180.0);
+            ctx->focal_length_h = ctx->grayscale.rows / tan(0.5 * ctx->config.field_of_view
+                * ctx->grayscale.cols / sqrt(pow(ctx->grayscale.rows, 2.0) + pow(ctx->grayscale.cols, 2.0))
+                * HT_PI / 180.0);
             //fprintf(stderr, "focal length = %f\n", ctx->focal_length_w);
         }
         //ht_draw_features(*ctx);
