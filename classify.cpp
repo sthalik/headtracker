6e846ff558efd7263d57aa629b2ce6e9a7dece66
Stanis³aw Halik <sthalik@misaki.pl>
2013-03-21 14:31:42 +0100

    More precise face detection

--- classify.cpp
+++ classify.cpp
@@ -9,9 +9,8 @@ bool ht_classify(CascadeClassifier &classifier, Mat& frame, Rect& ret) {
                                 seq,
                                 1.1,
                                 1,
-                                CV_HAAR_DO_CANNY_PRUNING | CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_DO_ROUGH_SEARCH,
-                                Size(40, 40),
-                                Size(240, 240));
+                                CV_HAAR_DO_CANNY_PRUNING | CV_HAAR_FIND_BIGGEST_OBJECT,
+                                Size(40, 40));
 
     if (seq.size() > 0)
         ret = seq[0];
