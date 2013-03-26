57afe6769a6d43c15e156597f03a73d098bd1cad
Stanislaw Halik <sthalik@misaki.pl>
2013-03-26 08:08:38 +0100

    Don't consume CPU time needlessly as much

--- features.cpp
+++ features.cpp
@@ -76,9 +76,9 @@ void ht_get_features(headtracker_t& ctx, model_t& model) {
     max_dist *= max_dist;
     max_3dist *= max_3dist;
     vector<KeyPoint> corners;
-    ORB detector = ORB(ctx.config.max_keypoints * 6,
-                       1.1f,
-                       12,
+    ORB detector = ORB(ctx.config.max_keypoints * 5,
+                       1.2f,
+                       8,
                        ctx.config.keypoint_quality,
                        0,
                        0,
