f6f6a9aa8ac6ec30088302eb1b7d8abdaef62de5
Stanisław Halik <sthalik@misaki.pl>
2013-04-12 14:40:24 +0200

    Use shorter permission value. No functional change.

--- ftnoir/compat.cpp
+++ ftnoir/compat.cpp
@@ -27,7 +27,7 @@ PortableLockedShm::PortableLockedShm(const char* shmName, const char* mutexName,
                  mapSize,
                  shmName);
     mem = MapViewOfFile(hMapFile,
-                        FILE_MAP_READ | FILE_MAP_WRITE,
+                        FILE_MAP_WRITE,
                         0,
                         0,
                         mapSize);
