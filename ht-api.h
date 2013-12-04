e3fb8b46eeee31636a1a963b077584a5969afdd6
Stanislaw Halik <sthalik@misaki.pl>
2013-12-04 03:59:05 +0100

    drop extern, breaks MSVC build with cv::Mat

--- ht-api.h
+++ ht-api.h
@@ -1,14 +1,9 @@
 #pragma once
 #ifndef HT_API
-#ifndef __cplusplus
-# define HT_EXTERN 
-#else
-# define HT_EXTERN extern "C" 
-#endif
 #   if defined(_WIN32) && !defined(MINGW)
-#     define HT_API(t) HT_EXTERN __declspec(dllexport) t __stdcall
+#     define HT_API(t) __declspec(dllexport) t __stdcall
 #   else
-#    define HT_API(t) HT_EXTERN t
+#    define HT_API(t) t
 #   endif
 #endif
 #if !defined(_WIN32) && !defined(_isnan)
